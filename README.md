bfb72c547eb36a22ffbb5deb5d1df47ce20fb175
isievwore <isievwore@systemspecs.com.ng>
2020-06-20 23:25:16 +0100

    Updated Badges

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
  # Andela_Teamwork With PostgreSQL
 
-[![Build Status](https://travis-ci.org/kelvinator07/Andela_Teamwork.svg?branch=develop)](https://travis-ci.org/kelvinator07/Andela_Teamwork)
+[![Build Status](https://travis-ci.com/kelvinator07/Teamwork-Backend.svg?branch=develop)](https://travis-ci.com/kelvinator07/Teamwork-Backend)
 
-[![Coverage Status](https://coveralls.io/repos/github/kelvinator07/Andela_Teamwork/badge.svg?branch=develop)](https://coveralls.io/github/kelvinator07/Andela_Teamwork?branch=develop)
+[![Coverage Status](https://coveralls.io/repos/github/kelvinator07/Teamwork-Backend/badge.svg?branch=develop)](https://coveralls.io/github/kelvinator07/Teamwork-Backend?branch=develop)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/9d24c1d56291de70ee57/maintainability)](https://codeclimate.com/github/kelvinator07/Teamwork-Backend/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/9d24c1d56291de70ee57/test_coverage)](https://codeclimate.com/github/kelvinator07/Teamwork-Backend/test_coverage)
+
+[![Build status](https://ci.appveyor.com/api/projects/status/uq689l19p0sgtdpk?svg=true)](https://ci.appveyor.com/project/kelvinator07/teamwork-backend)
+
 
 Project Overview: Teamwork is an internal social network for employees of an organization. The goal of this application is to facilitate more interaction between colleagues and promote team bonding.
 
---
+++ test/index.test.js
@@ -0,0 +1,49 @@
+import { expect, server, BASE_URL } from './setup';
+
+// describe('Test home routes', () => {
+//   it('get messages page', done => {
+//     server
+//       .get(`${BASE_URL}/messages`)
+//       .expect(200)
+//       .end((err, res) => {
+//         expect(res.status).to.equal(200);
+//         expect(res.body.messages).to.be.instanceOf(Array);
+//         res.body.messages.forEach(m => {
+//           expect(m).to.have.property('name');
+//           expect(m).to.have.property('message');
+//         });
+//         done();
+//       });
+//   });
+
+//   it('posts messages', done => {
+//     const data = { name: 'some name', message: 'new message' };
+//     server
+//       .post(`${BASE_URL}/messages`)
+//       .send(data)
+//       .expect(200)
+//       .end((err, res) => {
+//         expect(res.status).to.equal(201);
+//         expect(res.body.messages).to.be.instanceOf(Array);
+//         res.body.messages.forEach(m => {
+//           expect(m).to.have.property('id');
+//         });
+//         done();
+//       });
+//   });
+// });
+
+describe('Index page test', () => {
+  it('gets base url', done => {
+    server
+      .get(`${BASE_URL}/`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.message).to.equal(
+          'Environment variable is coming across.'
+        );
+        done();
+      });
+  });
+});
